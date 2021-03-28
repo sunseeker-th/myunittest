7612aecdcba45ae9056c1b27c9dc8f354121f607
tjuphao <juphaono@gmail.com>
2021-03-28 15:01:28 -0400

    Fixed lint for employee

--- employeee.py
+++ employeee.py
@@ -1,21 +1,25 @@
+"""Employeee class for unittest"""
 
-class Employee:
+class Employee(object):
     """ A sample Employee class"""
-    
+
     raise_amt = 1.05
-    
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
-        
+
     @property
     def email(self):
+        """ email """
         return '{}.{}.@email.com'.format(self.first, self.last)
-    
+
     @property
     def fullname(self):
-        return '{}.{}.format(self.first.self.last)'
-    
+        """ fullname """
+        return '{}.{}'.format(self.first, self.last)
+
     def apply_raise(self):
+        """ apply raise """
         self.pay = int(self.pay * self.raise_amt)
